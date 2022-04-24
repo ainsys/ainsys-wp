768fe7fc184244629fcedcad30dadc3ccf93646c
Ludmila Sviridova <ludmila@pressfoundry.com>
2022-04-24 14:11:31 +0300

    remove Alexandr's scss

--- wp-content/themes/ainsys/partials/blocks/theme-specific/img-txt-ul.php
+++ wp-content/themes/ainsys/partials/blocks/theme-specific/img-txt-ul.php
@@ -1,17 +1,16 @@
 <?php
 /**
- * CTA block.
+ * Image & Text with UL block.
  *
  * @package ainsys
  */
 
-$block_id = 'cta-' . $block['id'];
+$block_id = 'img-txt-ul' . $block['id'];
 if ( ! empty( $block['anchor'] ) ) {
 	$block_id = $block['anchor'];
 }
 
-// Create class attribute allowing for custom "className" and "align" values.
-$class_name = 'cta full-width';
+$class_name = 'img-txt-ul';
 if ( ! empty( $block['className'] ) ) {
 	$class_name .= ' ' . $block['className'];
 }
@@ -21,25 +20,50 @@ if ( ! empty( $block['align'] ) ) {
 if ( get_field( 'bg' ) ) {
 	$class_name .= ' bg-color-' . get_field( 'bg' );
 }
-$style       = '';
-$style_bg    = '';
-$style_align = '';
-if ( get_field( 'bg_img' ) ) {
-	$style_bg = 'background-image: url(' . wp_get_attachment_image_url( get_field( 'bg_img' ), 'full' ) . '); ';
+if ( get_field( 'align' ) ) {
+	$class_name .= ' img-align-' . get_field( 'align' );
 }
-if ( get_field( 'bg_alignment' ) ) {
-	$style_align = 'background-position: ' . get_field( 'bg_alignment' ) . ';';
-}
-if ( get_field( 'bg_img' ) || get_field( 'bg_alignment' ) ) {
-	$style = ' style="' . $style_bg . $style_align . '"';
-}
-
 ?>
 
-<section id="<?php echo esc_attr( $block_id ); ?>" class="<?php echo esc_attr( $class_name ); ?>"<?php echo $style; //phpcs:ignore ?>>
+<section  id="<?php echo esc_attr( $block_id ); ?>" class="<?php echo esc_attr( $class_name ); ?>">
 	<div class="container">
-		<div class="cta--inner">
-			<InnerBlocks />
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="img-txt-ul__img">
+					<?php echo wp_get_attachment_image( get_field( 'image' ), 'large', false, array( 'class' => '' ) ); ?>
+				</div>
+			</div>
+			<div class="col-lg-6">
+				<div class="img-txt-ul__txt">
+					<?php if ( get_field( 'title' ) ) { ?>
+						<h2><?php the_field( 'title' ); ?></h2>
+					<?php } ?>
+					<?php if ( get_field( 'description' ) ) { ?>
+						<div class="toggler__desc"><?php the_field( 'description' ); ?></div>
+					<?php } ?>
+					<?php if ( have_rows( 'ul' ) ) : ?>
+						<div class="img-txt-ul__list">
+							<?php
+							while ( have_rows( 'ul' ) ) :
+								the_row();
+								?>
+								<div class="img-txt-ul__item">
+									<?php if ( get_sub_field( 'image' ) ) { ?>
+										<div class="img-txt-ul__item__img">
+											<?php echo wp_get_attachment_image( get_sub_field( 'image' ), 'large', false, array( 'class' => '' ) ); ?>
+										</div>
+									<?php } ?>
+									<?php if ( get_sub_field( 'text' ) ) { ?>
+										<div class="img-txt-ul__item__text">
+											<?php the_sub_field( 'text' ); ?>
+										</div>
+									<?php } ?>
+								</div>
+							<?php endwhile; ?>
+						</div>
+					<?php endif; ?>
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
