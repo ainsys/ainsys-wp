3621d51cb805bf1ec3bc1c1662437e3fb71e16d0
Ludmila Sviridova <ludmila@pressfoundry.com>
2022-04-25 15:42:51 +0300

    header-footer

--- wp-content/themes/ainsys/footer.php
+++ wp-content/themes/ainsys/footer.php
@@ -7,106 +7,26 @@
 
 ?>
 
-			<footer class="py-5">
+			<footer class="footer">
 				<div class="container">
-					<div class="row">
-						<div class="col-lg-4 col-sm-12">
-							<a href="/" class="footer_logo mb-2 me-sm-5 mb-lg-0 text-dark text-decoration-none"></a>
-						</div>
 
-						<div class="col-lg-2 col-sm-6 col-6">
-							<h6>Клиентам</h6>
-							<ul class="nav flex-column">
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Как это работает?</a></li>
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Все Решения</a></li>
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Найти интегратора</a></li>
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">FAQ Q&A</a></li>
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Техническа поддержка</a></li>
-							</ul>
-						</div>
-
-						<div class="col-lg-2 col-sm-6 col-6">
-							<h6>Партнерам</h6>
-							<ul class="nav flex-column">
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Как с нами работать?</a></li>
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Стать Партнером</a></li>
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Техническая поддержка</a></li>
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Заказы клиентов на интеграцию</a></li>
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Q&A Партнерам</a></li>
-							</ul>
-						</div>
-
-						<div class="col-lg-2 col-sm-6 col-6">
-							<h6>Разработчикам</h6>
-							<ul class="nav flex-column">
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Как у нас работать?</a></li>
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">AINSYS Rest API</a></li>
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Конектор SDK</a></li>
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Developer Q&A</a></li>
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Техническая поддержка</a></li>
-							</ul>
-						</div>
-
-						<div class="col-lg-2 col-sm-6 col-6">
-							<h6>О компании</h6>
-							<ul class="nav flex-column">
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Вакансии</a></li>
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Корпоративный Блог</a></li>
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Контакты</a></li>
-								<li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-white">Презентация</a></li>
-							</ul>
-						</div>
-					</div>
-
-					<div class="py-4 my-4 border-top footer__bottom">
-						<ul class="list-unstyled list-docs list-links me-auto col-sm-12 col-md-12 col-lg-6">
-							<li class="nav-item ms-3 col-6 col-md-3 col-lg-2"><a class="footer__docs text-white" href="mailto:ainsys@gmail.com">ainsys@gmail.com</a></li>
-							<li class="nav-item ms-3 col-6 col-md-2 col-lg-2"><a class="footer__docs text-white" href="#">Cookies</a></li>
-							<li class="nav-item ms-3 col-6 col-md-3 col-lg-3"><a class="footer__docs text-white" href="#">Terms & Conditions</a></li>
-							<li class="nav-item ms-3 col-6 col-md-3 col-lg-2"><a class="footer__docs text-white" href="#">Privacy</a></li>
+					<div class="footer__content">
+						<ul class="footer__links">
+							<li><a href="mailto:ainsys@gmail.com">ainsys@gmail.com</a></li>
+							<li><a href="#">Cookies</a></li>
+							<li><a href="#">Terms & Conditions</a></li>
+							<li><a href="#">Privacy</a></li>
 						</ul>
 
-						<div class="footer__fl">
-							<p class="me-sm-4 footer__ltd">&copy; Stanform Europe Ltd., 2022</p>
+						<div class="footer__copyright-social">
+							<div class="footer__copyright">&copy; Stanform Europe Ltd., 2022</div>
 
-							<div class="footer__item">
-								<ul class="list-unstyled d-flex me-sm-4">
-									<li class="me-3 c-sm-6"><a class="" href="#">
-										<img src="./assets/img/soc__fb.svg" alt="fb" class="footer__soc__img">
-									</a></li>
-									<li class="me-3"><a class="" href="#">
-										<img src="./assets/img/soc__inst.svg" alt="fb" class="footer__soc__img">
-									</a></li>
-									<li class="me-3"><a class="" href="#">
-										<img src="./assets/img/soc__youtube.svg" alt="fb" class="footer__soc__img">
-									</a></li>
-									<li class="me-3"><a class="" href="#">
-										<img src="./assets/img/soc__tg.svg" alt="fb" class="footer__soc__img">
-									</a></li>
-									<li class="me-0"><a class="" href="#">
-										<img src="./assets/img/soc__vk.svg" alt="fb" class="footer__soc__img">
-									</a></li>
-								</ul>
-
-								<div class="dropdown__lang text-end">
-									<a href="#" class="dropdown__lang__item link-light text-decoration-none">
-										<p class="header__user__text">RU</p>
-									</a>
-									<ul class="submenu">
-										<li><a href="#" class="submenu__item">
-										<img src="./assets/img/lang__urk.svg" class="lang__img me-sm-2" alt="">
-										Українська</a>
-										</li>
-										<li><a href="#" class="submenu__item">
-										<img src="./assets/img/lang__ru.svg" class="lang__img me-sm-2" alt="">
-										Русский
-										</a></li>
-										<li><a href="#" class="submenu__item">
-										<img src="./assets/img/lang__usa.svg" class="lang__img me-sm-2" alt="">
-										English</a>
-										</li>
-									</ul>
-								</div>
+							<div class="footer__social">
+								<a class="" href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/components/soc__fb.svg" alt="fb" class="footer__soc__img"></a>
+								<a class="" href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/components/soc__inst.svg" alt="instagram" class="footer__soc__img"></a>
+								<a class="" href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/components/soc__youtube.svg" alt="youtube" class="footer__soc__img"></a>
+								<a class="" href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/components/soc__tg.svg" alt="tg" class="footer__soc__img"></a>
+								<a class="" href="#"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/components/soc__vk.svg" alt="vk" class="footer__soc__img"></a>
 							</div>
 						</div>
 					</div>
