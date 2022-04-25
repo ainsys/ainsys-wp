e00b19c394aa0b80db099a49ac5319ab18f26a94
Ludmila Sviridova <ludmila@pressfoundry.com>
2022-04-25 19:09:13 +0300

    hero-video

--- wp-content/themes/ainsys/partials/blocks/theme-specific/hero-video.php
+++ wp-content/themes/ainsys/partials/blocks/theme-specific/hero-video.php
@@ -24,12 +24,12 @@ if ( get_field( 'bg' ) ) {
 ?>
 
 <section id="<?php echo esc_attr( $block_id ); ?>" class="<?php echo esc_attr( $class_name ); ?>">
-	<div class="container">
-		<div class="hero-video__info">
+	<div class="hero-video__info">
+		<div class="container">
 			<div class="hero-video__title">
 				<h1>
 					<?php if ( get_field( 'title_top' ) ) { ?>
-						&gt;<span><?php the_field( 'title_top' ); ?></span>&lt;
+						&gt;<span id="Ticker"><?php the_field( 'title_top' ); ?></span>&lt;
 					<?php } ?>
 					<?php if ( get_field( 'title_bottom' ) ) { ?>
 						<br><?php the_field( 'title_bottom' ); ?>
@@ -39,10 +39,6 @@ if ( get_field( 'bg' ) ) {
 			<?php if ( get_field( 'text' ) ) { ?>
 				<p class="hero-video__text"><?php the_field( 'text' ); ?></p>
 			<?php } ?> 
-
-		</div>
-
-		<div class="hero-video__video">
 			<div class="toggler__switch">
 				<div class="toggler__switch__label active"><?php the_field( 'label_with' ); ?></div>
 				<div class="form-check form-switch">
@@ -51,38 +47,98 @@ if ( get_field( 'bg' ) ) {
 				<div class="toggler__switch__label"><?php the_field( 'label_without' ); ?></div>
 			</div>
 
-			<div class="toggler__screens">
-				<div class="toggler__screen active">
-					<div class="hero-video__video__wrapper">
-						<?php if ( have_rows( 'video_with' ) ) : ?>
-							<video loop muted autoplay preload="metadata"
-								<?php
-								while ( have_rows( 'video_with' ) ) {
-									the_row();
-									?>
-									<source src="<?php the_sub_field( 'video' ); ?>" type="video/<?php the_sub_field( 'type' ); ?>">
-								<?php } ?>
-							</video>
-						<?php endif; ?>
-					</div>
-				</div>
-				<div class="toggler__screen">
-					<div class="hero-video__video__wrapper">
-						<?php if ( have_rows( 'video_without' ) ) : ?>
-							<video loop muted autoplay preload="metadata"
-								<?php
-								while ( have_rows( 'video_without' ) ) {
-									the_row();
-									?>
-									<source src="<?php the_sub_field( 'video' ); ?>" type="video/<?php the_sub_field( 'type' ); ?>">
-								<?php } ?>
-							</video>
-						<?php endif; ?>
-					</div>
-				</div>
-			</div>
-
 		</div>
 	</div>
 
+	<div class="hero-video__video">
+
+		<div class="toggler__screens">
+			<div class="toggler__screen active">
+				<div class="hero-video__video__wrapper">
+					<?php if ( have_rows( 'video_with' ) ) : ?>
+						<video loop muted autoplay preload="metadata"
+							<?php
+							while ( have_rows( 'video_with' ) ) {
+								the_row();
+								?>
+								<source src="<?php the_sub_field( 'video' ); ?>" type="video/<?php the_sub_field( 'type' ); ?>">
+							<?php } ?>
+						</video>
+					<?php endif; ?>
+				</div>
+			</div>
+			<div class="toggler__screen">
+				<div class="hero-video__video__wrapper">
+					<?php if ( have_rows( 'video_without' ) ) : ?>
+						<video loop muted autoplay preload="metadata"
+							<?php
+							while ( have_rows( 'video_without' ) ) {
+								the_row();
+								?>
+								<source src="<?php the_sub_field( 'video' ); ?>" type="video/<?php the_sub_field( 'type' ); ?>">
+							<?php } ?>
+						</video>
+					<?php endif; ?>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
 </section>
+
+<?php if ( have_rows( 'animated_text' ) ) : ?>
+	<script type="text/javascript">
+		const CharTimeout = 50;
+		const StoryTimeout = 2000;
+
+		const Summaries = new Array();
+		const SiteLinks = new Array();
+
+		<?php
+		$i = 0;
+		while ( have_rows( 'animated_text' ) ) :
+			the_row();
+			?>
+			Summaries[<?php echo $i; ?>] = '<?php the_sub_field( 'text' ); ?>';
+		<?php
+		$i++;
+		endwhile;
+		?>
+
+		function runTheTicker(){
+			var myTimeout;  
+
+			if(CurrentLength == 0){
+				CurrentStory++;
+				CurrentStory      = CurrentStory % massiveItemCount;
+				StorySummary      = Summaries[CurrentStory].replace(/"/g,'-');      
+			}
+			AnchorObject.innerHTML = StorySummary.substring(0,CurrentLength) + znak();
+			if(CurrentLength != StorySummary.length){
+				CurrentLength++;
+				myTimeout = CharTimeout;
+			} else {
+				CurrentLength = 0;
+				myTimeout = StoryTimeout;
+			}
+			setTimeout("runTheTicker()", myTimeout);
+		}
+
+		function znak(){
+			if(CurrentLength == StorySummary.length) return "";
+			else return "|";
+		}
+		function startTicker(){
+			massiveItemCount =  Number(Summaries.length); 
+
+			CurrentStory     = -1;
+			CurrentLength    = 0;
+
+			AnchorObject     = document.getElementById("Ticker");
+			runTheTicker();     
+		}
+
+		startTicker();
+	</script>
+<?php endif; ?>
