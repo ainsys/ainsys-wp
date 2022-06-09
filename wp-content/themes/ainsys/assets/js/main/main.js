a98d5ddd59158518772bd03664c223acaafbaa16
vitstrong <vitstrong.wb1108@gmail.com>
2022-06-09 17:10:37 +0300

    iew

--- wp-content/themes/ainsys/assets/js/main/main.js
+++ wp-content/themes/ainsys/assets/js/main/main.js
@@ -1997,27 +1997,6 @@ function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
     return id;
   }
 
-  const cookieEl = document.querySelector('.cookie-block');
-  const okEl = document.querySelector('.ok');
-
-  okEl.addEventListener('click', () => {
-    cookieEl.style.display = 'none';
-  });
-
-  let cookies = () => {
-    if (!Cookies.get('hide-cookie')) {
-      setTimeout(() => {
-        cookieEl.style.display = 'block';
-      }, 1000);
-    }
-
-    Cookies.set('hide-cookie', 'true', {
-      expires: 30
-    });
-  }
-
-
-  cookies();
 
 })(jQuery);
 //# sourceMappingURL=main.js.map
