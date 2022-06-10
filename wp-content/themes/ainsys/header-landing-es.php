a29696e334f026c70bb0e1c35c5dbc3c5c053a45
Ignat <dev03.ainsys@gmail.com>
2022-06-10 17:54:16 +0300

    fix headers

--- wp-content/themes/ainsys/header-landing-es.php
+++ wp-content/themes/ainsys/header-landing-es.php
@@ -130,7 +130,7 @@
 								<div class="socials">
 									<div class="socials__wrapper">
 										<div class="header-landing__btn">
-											<a class="btn btn-main" href="https://es.ainsys.com/my-account/" onclick="gtag( 'event', 'buttom_1', {   'event_category' : 'ain',   'event_label' : 'b_1' }); ym(86987238,'reachGoal','button_1'); return true;">Solicitud de baja</a>
+											<a class="btn btn-main" href="https://es.ainsys.com/my-account/" onclick="gtag( 'event', 'buttom_1', {  'event_category' : 'ain',   'event_label' : 'b_1' }); ym(86987238,'reachGoal','button_1'); return true;">Solicitud de baja</a>
 										</div>
 										<ul class="socials__links">
 											<li  class="numbers__phone numbers__phone-disabled">
