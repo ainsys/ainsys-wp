fab4f27f3832d474bb298f1486649a3199197de4
Ignat <dev03.ainsys@gmail.com>
2022-06-11 13:54:36 +0300

    fix and bugs

--- wp-content/themes/ainsys/assets/js/main/main.js
+++ wp-content/themes/ainsys/assets/js/main/main.js
@@ -1065,35 +1065,6 @@ function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" =
 "use strict";
 
 (function ($) {
-  $('.country_close').click(function () {
-    $('#counrty').addClass('country-disabled');
-  });
-  $('.country_select').click(function () {
-    $(this).toggleClass('country-active');
-  });
-})(jQuery);
-"use strict";
-
-(function ($) {
-  document.body.classList.remove('no-js');
-  document.body.classList.add('js');
-})(jQuery);
-"use strict";
-
-(function ($) {
-  "use strict";
-
-  if (window['objectFitImages']) {
-    $(document).ready(function () {
-      window.objectFitImages(null, {
-        watchMQ: true
-      });
-    });
-  }
-})(jQuery);
-"use strict";
-
-(function ($) {
   var $siteHeader = $('.header__main');
   var $anouncementBar = $('.announcement-bar');
   var $menu = $('.header__menu__wrapper', $siteHeader);
@@ -1232,6 +1203,35 @@ function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" =
     }
     */
   });
+})(jQuery);
+"use strict";
+
+(function ($) {
+  $('.country_close').click(function () {
+    $('#counrty').addClass('country-disabled');
+  });
+  $('.country_select').click(function () {
+    $(this).toggleClass('country-active');
+  });
+})(jQuery);
+"use strict";
+
+(function ($) {
+  document.body.classList.remove('no-js');
+  document.body.classList.add('js');
+})(jQuery);
+"use strict";
+
+(function ($) {
+  "use strict";
+
+  if (window['objectFitImages']) {
+    $(document).ready(function () {
+      window.objectFitImages(null, {
+        watchMQ: true
+      });
+    });
+  }
 })(jQuery);
 "use strict";
 
