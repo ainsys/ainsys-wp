704caaf0481581befcf64cf9ce28e6bfa7113df6
Ignat <dev03.ainsys@gmail.com>
2022-06-23 13:13:12 +0300

    fix 404 page style

--- wp-content/themes/ainsys/partials/blocks/theme-specific/error.php
+++ wp-content/themes/ainsys/partials/blocks/theme-specific/error.php
@@ -1,22 +1,37 @@
 <?php
 /**
- * Error.
+ * Toggler block.
  *
  * @package ainsys
  */
 
-$block_id = 'error-' . $block['id'];
+$block_id = 'toggler-' . $block['id'];
 if ( ! empty( $block['anchor'] ) ) {
 	$block_id = $block['anchor'];
 }
 
 // Create class attribute allowing for custom "className" and "align" values.
-$class_name = 'content-block-slider';
+$class_name = 'toggler full-width';
 if ( ! empty( $block['className'] ) ) {
-    $class_name .= ' ' . $block['className'];
+	$class_name .= ' ' . $block['className'];
 }
 if ( ! empty( $block['align'] ) ) {
-    $class_name .= ' align' . $block['align'];
+	$class_name .= ' align' . $block['align'];
+}
+if ( get_field( 'bg_color' ) ) {
+	$class_name .= ' bg-color-' . get_field( 'bg_color' );
+}
+$style       = '';
+$style_bg    = '';
+$style_align = '';
+if ( get_field( 'bg_img' ) ) {
+	$style_bg = 'background-image: url(' . wp_get_attachment_image_url( get_field( 'bg_img' ), 'full' ) . '); ';
+}
+if ( get_field( 'bg_alignment' ) ) {
+	$style_align = 'background-position: ' . get_field( 'bg_alignment' ) . ';';
+}
+if ( get_field( 'bg_img' ) || get_field( 'bg_alignment' ) ) {
+	$style = ' style="' . $style_bg . $style_align . '"';
 }
 
 ?>
