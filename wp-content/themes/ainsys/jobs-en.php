49ba669ccd04512bf3b5b8e2f31b7c48761da9b9
Ignat <dev03.ainsys@gmail.com>
2022-06-24 16:42:19 +0300

    add shoblons jobs

---
+++ wp-content/themes/ainsys/jobs-en.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Jobs en.
+ *
+ * Template Name: Jobs en
+ *
+ * @package ainsys
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; }
+
+get_header( 'us' );
+
+the_post();
+
+the_content();
+
+get_footer( 'us' );
