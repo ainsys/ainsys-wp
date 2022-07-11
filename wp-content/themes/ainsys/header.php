9fb356505ca66a75356afadd9d41041c2fbab25e
Mobiss11 <mobiss11@yandex.ru>
2022-07-11 05:02:14 -0400

    active version

--- wp-content/themes/ainsys/header.php
+++ wp-content/themes/ainsys/header.php
@@ -75,6 +75,7 @@
 				var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
 			})(window, document, 'script', 'cloud.roistat.com', '687af597c04143baddf7137bcaa63754');
 		</script>
+            <script src="https://www.googleoptimize.com/optimize.js?id=OPT-WPK46P6"></script>
 
 		
 
