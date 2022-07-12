cb32e726ce34db6857a9791f47b147d481628834
Ignat <dev03.ainsys@gmail.com>
2022-07-12 14:09:26 +0300

    fix scroll

--- wp-content/themes/ainsys/src/js/blocks/common/href_scroll.js
+++ wp-content/themes/ainsys/src/js/blocks/common/href_scroll.js
@@ -2,6 +2,6 @@
     var myHash = location.hash; //получаем значение хеша
     location.hash = ''; //очищаем хеш
     if(myHash[1] != undefined){ //проверяем, есть ли в хеше какое-то значение
-        $('html, body').animate({scrollTop: $(myHash).offset().top}, 2500); //скроллим за полсекунды
+        $('html, body').animate({ scrollTop : $(myHash).offset().top}, 500); //скроллим за полсекунды
     };
 } )( jQuery ); 
